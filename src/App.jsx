4cf49379fb299b428de03cd56a4750da3e847da7
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-09-23 21:05:47 +0500

    EVN Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Config from './conf/config'
+
+function App() {
+// eslint-disable-next-line no-undef
+console.log(Config.appWriteUrl)
+  return (
+    <>
+    <h2>Hello World</h2>
+    </>
+  )
+}
+
+export default App
